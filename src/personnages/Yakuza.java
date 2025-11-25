2edc979d9df00bf8cd1af3840ed1e538e37e8439
Timeo-Barbara <timeo.barbara81@gmail.com>
2025-11-25 19:56:38 +0100

    le clan des Yakuzas

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -35,4 +35,10 @@ public class Yakuza extends Humain {
 		reputation = reputation + 1;
 		parler("Ce ronin pensait vraiment battre " + getNom() + " du clan de " + clan + ". Je l'ai dépouillé de ses " + gain + " sous");
 	}
+	
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Mon clan est celui de " + clan);
+	}
 }
